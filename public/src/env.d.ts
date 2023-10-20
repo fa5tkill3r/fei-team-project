a90dff7bc0ea640ec6de0838d25a5383cb74e325
Felix Vítek <felox8@gmail.com>
2023-10-20 15:13:18 +0200

    init strapi

--- public/src/env.d.ts
+++ public/src/env.d.ts
@@ -1,1 +1,5 @@
 /// <reference types="astro/client" />
+interface ImportMetaEnv {
+  readonly STRAPI_URL: string
+  readonly STRAPI_API_TOKEN: string
+}
---
+++ public/src/interfaces/Post.ts
@@ -0,0 +1,12 @@
+export default interface Post {
+  id: number
+  attributes: {
+    title: string
+    // description: string;
+    content: string
+    slug: string
+    // createdAt: string;
+    // updatedAt: string;
+    // publishedAt: string;
+  }
+}
