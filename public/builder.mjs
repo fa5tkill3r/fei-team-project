4c7db8738df5d22972a47e707647586d43b88821
Felix Vítek <felox8@gmail.com>
2023-10-21 13:17:21 +0200

    add simple builder script

---
+++ public/builder.mjs
@@ -0,0 +1,68 @@
+import { build } from 'astro'
+import { createServer } from 'node:http'
+
+const port = process.env.PORT || 3000
+let building = false
+
+function tryParse(str) {
+  try {
+    console.log('str', str)
+
+    return JSON.parse(str)
+  } catch (err) {
+    console.log('err', err)
+    return null
+  }
+}
+
+const server = createServer((req, res) => {
+  if (!req.url || req.method !== 'POST') {
+    res.writeHead(404)
+    res.end()
+    return
+  }
+
+  const buf = Buffer.alloc(512, 0)
+  let offset = 0
+
+  req.on('data', (chunk) => {
+    buf.set(chunk, offset)
+    offset += chunk.length
+  })
+
+  req.on('end', () => {
+    const body = tryParse(buf.slice(0, offset).toString('utf8'))
+
+    if (!body?.event) {
+      res.writeHead(400)
+      res.end()
+      return
+    }
+
+    if (building) {
+      res.writeHead(204)
+      res.end()
+      return
+    }
+
+    building = true
+
+    build({})
+      .then(() => {
+        building = false
+
+        res.writeHead(204)
+        res.end()
+      })
+  })
+
+  // const url = new URL(req.url)
+})
+
+server.listen(port, () => {
+  console.log('http://localhost:3000')
+})
+
+// await build({
+//   root: './'
+// })
