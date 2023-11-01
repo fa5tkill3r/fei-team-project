e4345d69d2f82a242339afabe29f7eb9b00a2123
Felix Vítek <felox8@gmail.com>
2023-11-01 11:48:25 +0100

    setup astro-i18n
    temporary, until this is merged https://github.com/withastro/roadmap/pull/734

--- public/src/interfaces/Post.ts
+++ public/src/interfaces/Post.ts
@@ -1,12 +1,7 @@
+import type Translation from './Translation'
+
 export default interface Post {
   id: number
-  // attributes: {
-    title: string
-    // description: string;
-    content: string
-    slug: string
-    // createdAt: string;
-    // updatedAt: string;
-    // publishedAt: string;
-  // }
+  slug: string
+  translations: Translation[]
 }
