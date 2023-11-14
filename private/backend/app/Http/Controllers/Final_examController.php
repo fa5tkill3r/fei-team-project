07fa9e5f80623e24c27d55a4b9859ef091a3696a
Felix Vítek <felox8@gmail.com>
2023-11-14 13:07:55 +0100

    setup webhook to rebuild public site

--- private/backend/app/Http/Controllers/Final_examController.php
+++ private/backend/app/Http/Controllers/Final_examController.php
@@ -4,10 +4,22 @@ namespace App\Http\Controllers;
 
 use App\Models\Final_exam;
 use Illuminate\Http\Request;
+use Spatie\WebhookServer\WebhookCall;
 use Tymon\JWTAuth\Facades\JWTAuth;
 
 class Final_examController extends Controller
 {
+    private function sendWebhook(string $event, $data): void
+    {
+        WebhookCall::create()
+            ->url(env('PUBLIC_WEBHOOK_URL'))
+            ->payload([
+                'event' => $event,
+                'data' => $data,
+            ])
+            ->dispatch();
+    }
+
     public function index()
     {
         return response()->json([
@@ -17,13 +29,15 @@ class Final_examController extends Controller
 
     public function store(Request $request)
     {
-        if(!JWTAuth::parseToken()->authenticate()) {
+        if (!JWTAuth::parseToken()->authenticate()) {
             return response()->json([
                 'message' => 'User not found',
-            ], 404);    
-        } 
+            ], 404);
+        }
 
         $final_exam = Final_exam::create($request->all());
+
+        $this->sendWebhook('final_exam.created', $final_exam);
 
         return response()->json([
             'data' => $final_exam,
@@ -39,14 +53,16 @@ class Final_examController extends Controller
 
     public function update(Request $request, $id)
     {
-        if(!JWTAuth::parseToken()->authenticate()) {
+        if (!JWTAuth::parseToken()->authenticate()) {
             return response()->json([
                 'message' => 'User not found',
-            ], 404);    
-        } 
+            ], 404);
+        }
 
         $final_exam = Final_exam::findOrFail($id);
         $final_exam->update($request->all());
+
+        $this->sendWebhook('final_exam.updated', $final_exam);
 
         return response()->json([
             'data' => $final_exam,
@@ -55,13 +71,16 @@ class Final_examController extends Controller
 
     public function destroy($id)
     {
-        if(!JWTAuth::parseToken()->authenticate()) {
+        if (!JWTAuth::parseToken()->authenticate()) {
             return response()->json([
                 'message' => 'User not found',
-            ], 404);    
-        } 
+            ], 404);
+        }
 
         Final_exam::findOrFail($id)->delete();
+
+        $this->sendWebhook('final_exam.deleted', $id);
+
         return response()->json(null, 204);
     }
 }
