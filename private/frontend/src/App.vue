e4c920d310df1bca01e07affcb92328ae2ba185a
Nicolas Ondráš <nicolasko999@gmail.com>
2023-11-14 08:59:00 +0100

    Setup tailwind, daisyui

--- private/frontend/src/App.vue
+++ private/frontend/src/App.vue
@@ -4,12 +4,41 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <div>
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <button
+      class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">
+      Button
+    </button>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
+  </div>
+  <div>
+    <div class="flex flex-col">
+      <div class="form-control w-52">
+        <label class="cursor-pointer label">
+          <span class="label-text">Remember me</span>
+          <input type="checkbox" class="toggle toggle-primary" checked />
+        </label>
+      </div>
+      <div class="form-control w-52">
+        <label class="cursor-pointer label">
+          <span class="label-text">Remember me</span>
+          <input type="checkbox" class="toggle toggle-secondary" checked />
+        </label>
+      </div>
+      <div class="form-control w-52">
+        <label class="cursor-pointer label">
+          <span class="label-text">Remember me</span>
+          <input type="checkbox" class="toggle toggle-accent" checked />
+        </label>
+      </div>
+    </div>
   </div>
   <HelloWorld msg="Vite + Vue" />
 </template>
