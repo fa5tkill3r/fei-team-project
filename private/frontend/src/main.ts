80fb21a85f3257ef477aa95f83acfe90df3a3628
Nicolas Ondráš <nicolasko999@gmail.com>
2023-11-22 22:35:02 +0100

    Basic setup

--- private/frontend/src/main.ts
+++ private/frontend/src/main.ts
@@ -1,5 +1,17 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import {createRouter, createWebHistory} from "vue-router";
+import routes from "./routes.ts";
 
-createApp(App).mount('#app')
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+
+
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
--- private/frontend/vite.config.ts
+++ private/frontend/vite.config.ts
@@ -3,5 +3,12 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+  plugins: [
+    vue(),
+  ],
 })
