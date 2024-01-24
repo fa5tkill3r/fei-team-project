3c1fc73cb1a3175430e30a20e84f05c971344796
Nicolas Ondráš <nicolasko999@gmail.com>
2024-01-24 23:24:00 +0100

    Prep animations

--- private/frontend/tailwind.config.js
+++ private/frontend/tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        fadeIn: 'fadeIn 0.5s ease-in-out',
+        fadeOut: 'fadeOut 0.5s ease-in-out',
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
+        fadeOut: {
+          '0%': { opacity: '1' },
+          '100%': { opacity: '0' },
+        },
+      },
+    },
   },
   plugins: [require('daisyui')],
   daisyui: {
