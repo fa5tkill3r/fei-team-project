007c3135c70cffc523642dcc1557d925469f91d6
RomanKois <koisroman@gmail.com>
2024-03-19 14:15:24 +0100

    just commenting

--- private/backend/database/migrations/2024_03_17_184833_additional_info_incident_table.php
+++ private/backend/database/migrations/2024_03_17_184833_additional_info_incident_table.php
@@ -16,11 +16,14 @@ return new class extends Migration
             $table->text('attack_solution')->nullable();
             $table->text('attack_description')->nullable();
             $table->text('attack_vector')->nullable();
+            //  zmenit na classu enum dynamicky zmenit a je to v stpcoch nie tak ako som si myslel
+            // nechat tu iba string na FE natiahnut z enumu stringy a podla toho upravit aj category aj type
             $table->enum('attack_category', ['attack', 'abuse', 'fraud', 'missusage'])->default('attack');
             $table->string('attack_type')->nullable();
-            //TODO: asi nie enum na ten typ ....
             $table->text('security_measures')->nullable();
             $table->text('notes')->nullable();
+            // TODO: pridat do komentara boolean ci je sucat solutionu
+            //TODO: pridat many opatrieni, (tabulka v docu)
             $table->timestamps();
         });
     }
